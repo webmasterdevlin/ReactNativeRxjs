1cd176e9fa632c4e6ca326254a94eb86ac184b31
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-08-06 09:18:34 +0200

    updated

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Text} from 'react-native-elements';
+import {of, combineLatest, forkJoin} from 'rxjs';
 import {catchError, map, mergeMap} from 'rxjs/operators';
 import {SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
 
@@ -7,16 +8,19 @@ import {get} from './app/api';
 import {Post} from './app/models/post';
 import {EndPoints} from './app/api/api-config';
 import {User} from './app/models/user';
+import {Album} from './app/models/album';
 
 const App = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [userName, setUserName] = useState('');
+  const [albums, setAlbums] = useState<Album[]>([]);
 
   useEffect(() => {
-    getPostsByUserId();
+    // tryMergeMap();
+    tryForkJoin();
   }, []);
 
-  const getPostsByUserId = () => {
+  const tryMergeMap = () => {
     get<User[]>(`${EndPoints.users}?username=Bret`)
       .pipe(
         map(users => {
@@ -39,6 +43,38 @@ const App = () => {
       });
   };
 
+  const tryForkJoin = () => {
+    get<User[]>(`${EndPoints.users}?username=Bret`)
+      .pipe(
+        map<User[], User>(users => {
+          const user = users[0];
+          setUserName(user.username);
+
+          return user;
+        }),
+        mergeMap<User, any>(user => {
+          const postsData2 = get<Post[]>(
+            `${EndPoints.posts}?userId=${user.id}`,
+          );
+          const albumsData2 = get<Album[]>(
+            `${EndPoints.albums}?userId=${user.id}`,
+          );
+
+          return forkJoin<any>([postsData2, albumsData2]);
+        }),
+        catchError(err => {
+          console.log(err);
+          return err;
+        }),
+      )
+      .subscribe(result => {
+        console.log('result[0]::', JSON.stringify(result[0], null, 2));
+        console.log('result[1]::', JSON.stringify(result[1], null, 2));
+        setPosts(result[0]);
+        setAlbums(result[1]);
+      });
+  };
+
   return (
     <SafeAreaView>
       <StatusBar />
@@ -48,16 +84,34 @@ const App = () => {
             backgroundColor: '#FFF',
             padding: 20,
           }}>
-          <Text h2>{userName}</Text>
+          <View
+            style={{
+              backgroundColor: '#FFF',
+              padding: 20,
+            }}>
+            <Text h2>Merge Map</Text>
+          </View>
+          {posts?.map(post => (
+            <View key={post.id}>
+              <Text h3>{post.title}</Text>
+            </View>
+          ))}
         </View>
         <View
           style={{
             backgroundColor: '#FFF',
             padding: 20,
           }}>
-          {posts?.map(post => (
-            <View key={post.id}>
-              <Text h3>{post.title}</Text>
+          <View
+            style={{
+              backgroundColor: '#FFF',
+              padding: 20,
+            }}>
+            <Text h2>With Fork Join</Text>
+          </View>
+          {albums?.map(album => (
+            <View key={album.id}>
+              <Text h3>{album.title}</Text>
             </View>
           ))}
         </View>
